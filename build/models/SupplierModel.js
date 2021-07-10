6342f88c33a9d402ff13a43cca64a37ecb630db9
dikiamust <agungmustaqimdiki@gmail.com>
2021-07-10 22:02:00 +0700

    debuging

--- build/models/SupplierModel.js
+++ build/models/SupplierModel.js
@@ -3,7 +3,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Supplier = void 0;
 const mongoose_1 = __importDefault(require("mongoose"));
 const supplierSchema = new mongoose_1.default.Schema({
     supplierName: { type: String, required: true, unique: true },
--- build/models/UserModel.js
+++ build/models/UserModel.js
@@ -3,7 +3,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.User = void 0;
 const mongoose_1 = __importDefault(require("mongoose"));
 const userSchema = new mongoose_1.default.Schema({
     username: { type: String },
